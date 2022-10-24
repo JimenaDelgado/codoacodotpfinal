40379e7112de3610b6bf51cec93f94b494c45cd5
JimenaDelgado <113731153+JimenaDelgado@users.noreply.github.com>
2022-10-24 15:58:57 -0300

    Update contacto.js

--- TPO CODO A CODO 2 2/contacto.js
+++ TPO CODO A CODO 2 2/contacto.js
@@ -1,16 +1,3 @@
-const apiUrl = " https://api.covidtracking.com/v1/us/daily.json"
-
-async function obtenerPosicion(){
-    const response = await fetch(apiUrl)
-    const data = await response.json()
-    console.log(data);
-
-    document.getElementById("fecha").innerHTML = data.fecha
-    document.getElementById("muerte").innerHTML = data["muerte"]
-
-}
-
-obtenerPosicion()
 function valida_envia(){
     
     if (document.fvalida.nombre.value.length==0){
